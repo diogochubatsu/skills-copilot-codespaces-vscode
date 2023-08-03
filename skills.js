9a124fa6e154b82029f0af8385f2abd516be76ae
diogochubatsu <diogochubatsu@gmail.com>
2023-08-03 00:37:24 +0000

    Copilot firts commit

---
+++ skills.js
@@ -0,0 +1,11 @@
+// Status: Complete
+
+// 1. Create a function that takes in a number and returns the number times two. Then run the function and print the result.
+
+function calculateNumbers(var1, var2) {
+    console.log(var1 + var2);
+}   // 3    
+
+
+
+
