42031535e13b72772bdaf12fd1d017b714c94fc7
diogochubatsu <diogochubatsu@gmail.com>
2023-08-03 01:03:50 +0000

    Copilot second commit

--- member.js
+++ member.js
@@ -1,16 +1,6 @@
 function skillMember() {
-    var member = {
-        name: 'John',
-        age: 30,
-        skills: ['JavaScript', 'CSS', 'HTML']
-    };
-
-    member.sayHi = function () {
-        console.log('Hi, I am ' + this.name);
-    };
-
-    return member;
+  return {
+    restrict: 'E',
+    templateUrl: 'skill-member.html'
+  };
 }
-
-var member = skillMember();
-member.sayHi(); // Hi, I am John
